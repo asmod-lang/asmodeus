3f113ebbea3263411cc9ee71363d71ccac974913
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-27 20:41:28 +0200

    hephasm directive processor

---
+++ hephasm/src/directive.rs
@@ -0,0 +1,45 @@
+//! directive processing (RST, RPA, etc.)
+
+use crate::error::AssemblerError;
+use crate::operand::OperandResolver;
+use parseid::ast::Directive;
+
+pub struct DirectiveProcessor {
+    operand_resolver: OperandResolver,
+}
+
+impl DirectiveProcessor {
+    pub fn new() -> Self {
+        Self {
+            operand_resolver: OperandResolver::new(),
+        }
+    }
+
+    pub fn assemble_directive(&self, directive: &Directive, memory: &mut [u16], current_address: usize) -> Result<(), AssemblerError> {
+        match directive.name.to_uppercase().as_str() {
+            "RST" => {
+                let value = if directive.arguments.is_empty() {
+                    0
+                } else {
+                    let signed_value = self.operand_resolver.parse_signed_number(&directive.arguments[0], directive.line)?;
+                    signed_value as u16
+                };
+                memory[current_address] = value;
+            }
+            "RPA" => {
+                memory[current_address] = 0;
+            }
+            _ => {
+                // unknown directive - ignoring for now
+                // TODO: handle unknown directives
+            }
+        }
+        Ok(())
+    }
+}
+
+impl Default for DirectiveProcessor {
+    fn default() -> Self {
+        Self::new()
+    }
+}
