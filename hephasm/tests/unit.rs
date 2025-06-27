03f6eba34e4e7904962827a397f4bc4910ee8ba9
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-27 20:42:56 +0200

    unit test interface for hephasm

---
+++ hephasm/tests/unit.rs
@@ -0,0 +1,11 @@
+// unit tests for hephasm assembler 
+
+mod unit {
+    pub mod instruction_tests;
+    pub mod addressing_tests;
+    pub mod label_tests;
+    pub mod directive_tests;
+    pub mod macro_tests;
+    pub mod error_tests;
+    pub mod number_format_tests;
+}
