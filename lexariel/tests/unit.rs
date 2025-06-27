5d1c27f0da44dc2c20acb8f2237e3ab5d6f34ae1
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-27 20:58:47 +0200

    unit tests interface for lexariel

---
+++ lexariel/tests/unit.rs
@@ -0,0 +1,10 @@
+mod unit {
+    pub mod basic_tokenization_tests;
+    pub mod keyword_tests;
+    pub mod identifier_tests;
+    pub mod number_tests;
+    pub mod punctuation_tests;
+    pub mod comment_tests;
+    pub mod position_tests;
+    pub mod error_tests;
+}
