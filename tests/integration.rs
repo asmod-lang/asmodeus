df3757102145f08bc335a7b7069a842eaeec2242
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-28 01:19:05 +0200

    core integration tests interface

---
+++ tests/integration.rs
@@ -0,0 +1,10 @@
+mod integration {
+    pub mod pipeline_tests;
+    pub mod error_handling_tests;
+    pub mod roundtrip_tests;
+    pub mod cli_tests;
+    pub mod debugger_tests;
+    pub mod character_io_tests;
+    pub mod example_tests;
+    pub mod complex_algorithm_tests;
+}
