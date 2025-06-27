46020dcdd4b6789709e0420bb6f16e1b9042eb8f
Szymon Wilczek <kazikwilczek7@gmail.com>
2025-06-28 00:57:31 +0200

    parseid unit tests interface

---
+++ parseid/tests/unit.rs
@@ -0,0 +1,12 @@
+// Główny plik testów jednostkowych dla parseid
+
+mod unit {
+    pub mod basic_parsing_tests;
+    pub mod instruction_tests;
+    pub mod addressing_mode_tests;
+    pub mod label_tests;
+    pub mod directive_tests;
+    pub mod macro_tests;
+    pub mod error_tests;
+    pub mod value_format_tests;
+}
